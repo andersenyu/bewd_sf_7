07143d639bef0e9dce5ab032b57ee8f0c7655e71
Andersen Yu <andersenyu0716@gmail.com>
2014-12-08 18:35:48 -0800

    added mod

--- 04_Collections_APIs/starter_code/ex_teddit_api_news.rb
+++ 04_Collections_APIs/starter_code/ex_teddit_api_news.rb
@@ -5,3 +5,20 @@
 
 # Pull the json, parse it and then make a new story hash out of each story(Title, Category, Upvotes)
 # Add each story to an array and display your "Front page"
+
+require 'json'
+require 'rest-client'
+
+reddit_json = JSON.load(RestClient.get('http://www.reddit.com/.json'))
+
+stories = reddit_json["data"]["children"]
+
+collect = {}
+
+collect = stories.each do |story|
+	puts story["data"]["subreddit"].capitalize
+	puts story["data"]["title"]
+	puts story["data"]["score"]
+	puts
+end
+
